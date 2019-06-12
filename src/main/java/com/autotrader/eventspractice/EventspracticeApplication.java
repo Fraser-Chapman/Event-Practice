f793d02350a31fd1b8c1e67cff95a10c7eba4179
fraser.chapman <fraser.chapman@autotrader.co.uk>
2019-06-12 11:02:52 +0100

    initial commit

---
+++ src/main/java/com/autotrader/eventspractice/EventspracticeApplication.java
@@ -0,0 +1,13 @@
+package com.autotrader.eventspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventspracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventspracticeApplication.class, args);
+	}
+
+}
