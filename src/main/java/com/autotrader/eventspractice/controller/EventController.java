38d33399c9b5d7f9b9682a5487a418eb0a7829de
fraser.chapman <fraser.chapman@autotrader.co.uk>
2019-06-12 16:30:59 +0100

    basic front end

--- src/main/java/com/autotrader/eventspractice/controller/EventController.java
+++ src/main/java/com/autotrader/eventspractice/controller/EventController.java
@@ -5,12 +5,10 @@ import com.autotrader.eventspractice.entity.Events;
 import com.autotrader.eventspractice.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:63342")
 @RequestMapping(value = "/api/event")
 public class EventController {
 
--- src/main/java/com/autotrader/eventspractice/controller/UserController.java
+++ src/main/java/com/autotrader/eventspractice/controller/UserController.java
@@ -4,12 +4,10 @@ import com.autotrader.eventspractice.entity.User;
 import com.autotrader.eventspractice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:63342")
 @RequestMapping(value = "/api/user")
 public class UserController {
 
